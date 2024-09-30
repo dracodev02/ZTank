aadd0b40c9f277cc9d47d4608ebe7ea43bcf116f
quynhuw <qinhuuuuu@gmail.com>
2024-10-01 00:49:33 +0700

    add about us section

--- app/home/AboutUs.tsx
+++ app/home/AboutUs.tsx
@@ -1,4 +1,28 @@
+import ImageComp from "@/components/ImageComp";
+import background from "@/public/img/about-us-bg.png";
+
 const AboutUs = () => {
-  return <div>hehe</div>;
+  return (
+    <div className="pt-24 relative">
+      <div className="absolute top-0 left-0 w-screen h-screen -z-[1]">
+        <ImageComp
+          src={background.src}
+          className="object-cover w-screen h-screen"
+        />
+      </div>
+      <div className=" w-full max-w-[1520px] mx-auto pt-[203px] flex flex-col gap-3">
+        <h1 className="text-[86px] font-anton text-white uppercase inline-grid">
+          <span>Epic Battles,</span>
+          <span>Unstoppable Legends</span>
+          <span className="text-orange">on Starknet</span>
+        </h1>
+        <p className="text-[26px] leading-8 font-sf-pro-display max-w-[488px]">
+          Victory Awaits. Powered by Starknet.
+          <br /> ZTANK delivers fast, secure gameplay where domination brings
+          real blockchain rewards.
+        </p>
+      </div>
+    </div>
+  );
 };
 export default AboutUs;
--- components/Header.tsx
+++ components/Header.tsx
@@ -22,9 +22,9 @@ const Header = () => {
   ];
 
   return (
-    <div className="w-screen bg-[#000D12]  fixed top-0 left-0 z-10 right-0 font-inter text-[20px] leading-6">
-      <div className="w-full max-w-[1520px] mx-auto flex justify-between items-center">
-        <ImageComp src={logo.src} className="h-24 aspect-square" />
+    <div className="w-screen bg-[#000D12] h-[96px] fixed top-0 left-0 z-10 right-0 font-inter text-[20px] leading-6">
+      <div className="w-full h-full max-w-[1520px] mx-auto flex justify-between items-center">
+        <ImageComp src={logo.src} className="aspect-square h-full" />
         <div className="flex gap-10">
           {items.map((item, index) => {
             return <div key={index}>{item.name}</div>;
